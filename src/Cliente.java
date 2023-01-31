a33548ca9bc4e4d40fe32c08c224856c5d7eafe2
David Perea <david.perea@iesnervion.es>
2023-01-31 19:57:14 +0100

    2 furula

--- src/Cliente.java
+++ src/Cliente.java
@@ -13,32 +13,39 @@ public class Cliente {
             // 2 - Creación del socket UDP
             System.out.println("(Cliente): Creando el socket...");
             DatagramSocket socket = new DatagramSocket();
-
+            System.out.println("(Cliente) Enviando datagrama1...");
             String mensaje = "diego";
-            String mensaje2 ="diego";
-
             // 3 - Generación del datagrama
             System.out.println("(Cliente): Creando datagrama...");
             byte[] buffer = mensaje.getBytes();
             DatagramPacket packet = new DatagramPacket(buffer, buffer.length, direccion, 42500);
+            socket.send(packet);
 
+            String mensaje2 ="abcde";
             byte[] buffer2 = mensaje2.getBytes();
-            DatagramPacket packet2 = new DatagramPacket(buffer, buffer2.length, direccion, 42500);
+            DatagramPacket packet2 = new DatagramPacket(buffer2, buffer2.length, direccion, 42500);
 
             // 4 - Envío del datagrama a través de send
-            System.out.println("(Cliente) Enviando datagrama...");
-            socket.send(packet);
+            System.out.println("(Cliente) Enviando datagrama2...");
             socket.send(packet2);
 
 
             // 5 - Recepción de la respuesta
             System.out.println("(Cliente) Recibiendo respuesta...");
-            byte[] bufferEntrada = new byte[128];
+            byte[] bufferEntrada = new byte[64];
             DatagramPacket packetRecibir = new DatagramPacket(bufferEntrada, bufferEntrada.length);
             socket.receive(packetRecibir);
             String mensajeRecibido =  new String(packetRecibir.getData());
             System.out.println(mensajeRecibido.trim());
 
+
+            // 5 - Recepción de la respuesta
+            System.out.println("(Cliente) Recibiendo respuesta...");
+            byte[] bufferEntrada2 = new byte[64];
+            DatagramPacket packetRecibir2 = new DatagramPacket(bufferEntrada, bufferEntrada.length);
+            socket.receive(packetRecibir2);
+            String mensajeRecibido2 =  new String(packetRecibir.getData());
+            System.out.println(mensajeRecibido2.trim());
 
             // 6 - Cierre del socket
             System.out.println("(Cliente): Cerrando conexión...");
--- src/Servidor.java
+++ src/Servidor.java
@@ -14,15 +14,18 @@ public class Servidor {
             // 2 - Crear array de bytes que actuará de buffer
             byte[] buffer = new byte[64];
             byte[] buffer2 = new byte[64];
+
             // 3 - Creación de datagrama con la clase DatagramPacket
             DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-            DatagramPacket packet2 = new DatagramPacket(buffer2, buffer2.length);
             // 4 - Recepción del datagrama mediante el método receive
             socket.receive(packet);
+
+
+            DatagramPacket packet2 = new DatagramPacket(buffer2, buffer2.length);
             socket.receive(packet2);
 
-            String mensaje = "Hola "+ new String(packet.getData()) + " eres el " + new String(packet2.getData());
-
+            String mensaje = "Hola "+ new String(packet.getData()).trim() ;
+            String mensaje2 = " eres el " + new String(packet2.getData()).trim();
             // 5 - Generación y envío de la respuesta
             System.out.println("(Servidor): Enviando datagrama...");
             byte[] bufferSalida = mensaje.getBytes();
@@ -30,6 +33,15 @@ public class Servidor {
             DatagramPacket packetSalida = new DatagramPacket(bufferSalida, bufferSalida.length, packet.getAddress(), packet.getPort());
 
             socket.send(packetSalida);
+
+            // 5 - Generación y envío de la respuesta
+            System.out.println("(Servidor): Enviando datagrama...");
+            byte[] bufferSalida2 = mensaje2.getBytes();
+
+            DatagramPacket packetSalida2 = new DatagramPacket(bufferSalida2, bufferSalida2.length, packet2.getAddress(), packet.getPort());
+
+            socket.send(packetSalida2);
+
 
             // 6 - Cierre del socket
             System.out.println("(Servidor): Cerrando la conexión...");
